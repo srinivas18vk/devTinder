38d37135943bfa71290db1cfb01a99dbb1310fdf
srinivas18vk <srinivas18vk@gmail.com>
2025-06-21 10:21:40 +0530

    test commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = 2025;
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
+console.log("Server is running on port " + PORT);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
